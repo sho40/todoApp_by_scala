4bc44fe6450ace85ff2f5768cb935f7b587cbb37
sho40 <kanazawa.sho40@gmail.com>
2020-03-02 00:22:38 +0900

    Create TodoController.scala

---
+++ app/controllers/TodoController.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import javax.inject._
+import play.api.mvc._
+
+import play.api.data._
+import play.api.data.Forms._
+import services._
+
+
+class TodoController @Inject()(todoService: TodoService,mcc: MessagesControllerComponents)
+extends MessagesAbstractController(mcc) {
+  def helloworld() = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok("Hello World")
+  }
+
+  def list() = Action { implicit request: MessagesRequest[AnyContent] =>
+    val items: Seq[Todo] = todoService.list()
+    Ok(views.html.list(items))
+  }
+
+  val todoForm: Form[String] = Form("name" -> nonEmptyText)
+
+  def todoNew = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.createForm(todoForm))
+  }
+
+  def todoAdd = Action { implicit request: MessagesRequest[AnyContent] =>
+    val name: String = todoForm.bindFromRequest().get
+    todoService.insert(Todo(name))
+    Redirect(routes.TodoController.list())
+  }
+}
+
+
+
